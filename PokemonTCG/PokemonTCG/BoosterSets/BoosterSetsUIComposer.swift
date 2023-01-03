4e4bdc5bf10abef184de5ba4d9b5dba1778446fe
marlon ansale <luffydgreat90@gmail.com>
2023-01-03 23:27:39 +0000

    Added `WeakRefVirtualProxy`.

---
+++ PokemonTCG/PokemonTCG/BoosterSets/BoosterSetsUIComposer.swift
@@ -0,0 +1,11 @@
+//
+//  BoosterSetsUIComposer.swift
+//  PokemonTCG
+//
+//  Created by Marlon Ansale on 1/3/23.
+//
+
+import UIKit
+import Combine
+import PokemonFeed
+import PokemoniOS
---
+++ PokemonTCG/PokemonTCG/Shared/WeakRefVirtualProxy.swift
@@ -0,0 +1,36 @@
+//
+//  WeakRefVirtualProxy.swift
+//  PokemonTCG
+//
+//  Created by Marlon Ansale on 1/3/23.
+//
+
+import UIKit
+import PokemonFeed
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: ResourceErrorView where T: ResourceErrorView {
+    func display(_ viewModel: ResourceErrorViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: ResourceLoadingView where T: ResourceLoadingView {
+    func display(_ viewModel: ResourceLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: ResourceView where T: ResourceView, T.ResourceViewModel == UIImage {
+    func display(_ model: UIImage) {
+        object?.display(model)
+    }
+}
+
