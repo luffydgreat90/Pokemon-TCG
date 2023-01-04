70bf171ca83b22ebc02ca8ab42e9469e0d38208e
marlon ansale <luffydgreat90@gmail.com>
2023-01-04 13:28:50 +0000

    Added `BoosterSetStore`.

---
+++ PokemonFeed/PokemonFeed/BoosterSets/Cache/BoosterSetStore.swift
@@ -0,0 +1,33 @@
+//
+//  BoosterSetStore.swift
+//  PokemonFeed
+//
+//  Created by Marlon Ansale on 1/4/23.
+//
+
+import Foundation
+
+public typealias CachedBoosterSet = (feed: [LocalBoosterSet], timestamp: Date)
+
+public protocol BoosterSetStore {
+    typealias DeletionResult = Result<Void, Error>
+    typealias DeletionCompletion = (DeletionResult) -> Void
+    
+    typealias InsertionResult = Result<Void, Error>
+    typealias InsertionCompletion = (InsertionResult) -> Void
+
+    typealias RetrievalResult = Result<CachedBoosterSet?, Error>
+    typealias RetrievalCompletion = (RetrievalResult) -> Void
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    func insert(_ feed: [LocalBoosterSet], timestamp: Date, completion: @escaping InsertionCompletion)
+
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    func retrieve(completion: @escaping RetrievalCompletion)
+}
---
+++ PokemonFeed/PokemonFeed/BoosterSets/Cache/LocalBoosterSetLoader.swift
@@ -0,0 +1,8 @@
+//
+//  LocalBoosterSetLoader.swift
+//  PokemonFeed
+//
+//  Created by Marlon Ansale on 1/4/23.
+//
+
+import Foundation
