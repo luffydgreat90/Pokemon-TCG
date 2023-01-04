1ec940aa749ef564202f6c6aff734d16edd03d4e
marlon ansale <luffydgreat90@gmail.com>
2023-01-04 22:29:19 +0000

    Added `CardEndPoint` with tests.

---
+++ PokemonFeed/PokemonFeed/Cards/API/CardEndPoint.swift
@@ -0,0 +1,19 @@
+//
+//  CardEndPoint.swift
+//  PokemonFeed
+//
+//  Created by Marlon Ansale on 1/4/23.
+//
+
+import Foundation
+
+public enum CardEndPoint {
+    case get(_ id:String)
+
+    public func url(baseURL: URL) -> URL {
+        switch self {
+        case let .get(id):
+            return URL(string: "\(baseURL.absoluteString)/cards?q=set.id:\(id)")!
+        }
+    }
+}
---
+++ PokemonFeed/PokemonFeedTests/Cards/API/CardEndPointTests.swift
@@ -0,0 +1,21 @@
+//
+//  CardEndPointTests.swift
+//  PokemonFeedTests
+//
+//  Created by Marlon Ansale on 1/4/23.
+//
+
+import XCTest
+import PokemonFeed
+
+class CardEndPointTests: XCTestCase {
+    func test_feed_endpointURL() {
+        let id = "base1"
+        let baseURL = URL(string: "http://base-url.com")!
+        let received = CardEndPoint.get(id).url(baseURL: baseURL)
+        let expected = URL(string: "http://base-url.com/cards?q=set.id:\(id)")!
+
+        XCTAssertEqual(received.absoluteString, expected.absoluteString)
+    }
+
+}
