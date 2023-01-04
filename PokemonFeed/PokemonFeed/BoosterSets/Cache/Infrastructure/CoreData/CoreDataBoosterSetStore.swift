9963d41decb656510ba12bf8c8517a815083b92a
marlon ansale <luffydgreat90@gmail.com>
2023-01-04 14:20:50 +0000

    Added `CoreDataBoosterSetStore`.

--- PokemonFeed/PokemonFeed/BoosterSets/Cache/Infrastructure/CoreData/CoreDataBoosterSetStore.swift
+++ PokemonFeed/PokemonFeed/BoosterSets/Cache/Infrastructure/CoreData/CoreDataBoosterSetStore.swift
@@ -5,4 +5,46 @@
 //  Created by Marlon Ansale on 1/4/23.
 //
 
-import Foundation
+import CoreData
+
+public final class CoreDataBoosterSetStore {
+    private static let modelName = "BoosterSetStore"
+    private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataBoosterSetStore.self))
+    
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
+
+    enum StoreError: Error {
+        case modelNotFound
+        case failedToLoadPersistentContainer(Error)
+    }
+    
+    public init(storeURL: URL) throws {
+        guard let model = CoreDataBoosterSetStore.model else {
+            throw StoreError.modelNotFound
+        }
+
+        do {
+            container = try NSPersistentContainer.load(name: CoreDataBoosterSetStore.modelName, model: model, url: storeURL)
+            context = container.newBackgroundContext()
+        } catch {
+            throw StoreError.failedToLoadPersistentContainer(error)
+        }
+    }
+    
+    func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        let context = self.context
+        context.perform { action(context) }
+    }
+
+    private func cleanUpReferencesToPersistentStores() {
+        context.performAndWait {
+            let coordinator = self.container.persistentStoreCoordinator
+            try? coordinator.persistentStores.forEach(coordinator.remove)
+        }
+    }
+
+    deinit {
+        cleanUpReferencesToPersistentStores()
+    }
+}
