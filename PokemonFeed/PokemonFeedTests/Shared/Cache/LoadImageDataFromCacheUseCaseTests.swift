af0762f13c7422dd6fa3f374102061e4ddabae89
marlon ansale <luffydgreat90@gmail.com>
2023-01-07 21:12:07 +0000

    Added `SuperTypeTests`.

--- PokemonFeed/PokemonFeedTests/Shared/Cache/LoadImageDataFromCacheUseCaseTests.swift
+++ PokemonFeed/PokemonFeedTests/Shared/Cache/LoadImageDataFromCacheUseCaseTests.swift
@@ -1,5 +1,5 @@
 //
-//  LoadBoosterSetImageDataFromCacheUseCaseTests.swift
+//  LoadImageDataFromCacheUseCaseTests.swift
 //  PokemonFeedTests
 //
 //  Created by Marlon Ansale on 1/4/23.
@@ -8,7 +8,7 @@
 import XCTest
 import PokemonFeed
 
-class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
+class LoadImageDataFromCacheUseCaseTests: XCTestCase {
     func test_init_doesNotMessageStoreUponCreation() {
         let (_, store) = makeSUT()
         
@@ -67,7 +67,7 @@ class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
 
     func test_loadImageDataFromURL_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
         let store = ImageDataStoreSpy()
-        var sut: ImageDataLoader? = LocalBoosterSetImageDataLoader(store: store)
+        var sut: ImageDataLoader? = LocalImageDataLoader(store: store)
 
         var received = [ImageDataLoader.Result]()
         _ = sut?.loadImageData(from: anyURL()) { received.append($0) }
@@ -80,27 +80,27 @@ class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
     
     // MARK: - Helpers
 
-    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalBoosterSetImageDataLoader, store: ImageDataStoreSpy) {
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalImageDataLoader, store: ImageDataStoreSpy) {
         let store = ImageDataStoreSpy()
-        let sut = LocalBoosterSetImageDataLoader(store: store)
+        let sut = LocalImageDataLoader(store: store)
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
     }
 
     private func failed() -> ImageDataLoader.Result {
-        return .failure(LocalBoosterSetImageDataLoader.LoadError.failed)
+        return .failure(LocalImageDataLoader.LoadError.failed)
     }
 
     private func notFound() -> ImageDataLoader.Result {
-        return .failure(LocalBoosterSetImageDataLoader.LoadError.notFound)
+        return .failure(LocalImageDataLoader.LoadError.notFound)
     }
 
     private func never(file: StaticString = #filePath, line: UInt = #line) {
         XCTFail("Expected no no invocations", file: file, line: line)
     }
     
-    private func expect(_ sut: LocalBoosterSetImageDataLoader, toCompleteWith expectedResult: ImageDataLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+    private func expect(_ sut: LocalImageDataLoader, toCompleteWith expectedResult: ImageDataLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
         let exp = expectation(description: "Wait for load completion")
 
         _ = sut.loadImageData(from: anyURL()) { receivedResult in
@@ -108,8 +108,8 @@ class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
             case let (.success(receivedData), .success(expectedData)):
                 XCTAssertEqual(receivedData, expectedData, file: file, line: line)
 
-            case (.failure(let receivedError as LocalBoosterSetImageDataLoader.LoadError),
-                  .failure(let expectedError as LocalBoosterSetImageDataLoader.LoadError)):
+            case (.failure(let receivedError as LocalImageDataLoader.LoadError),
+                  .failure(let expectedError as LocalImageDataLoader.LoadError)):
                 XCTAssertEqual(receivedError, expectedError, file: file, line: line)
 
             default:
--- PokemonFeed/PokemonFeedTests/Shared/Domain/SuperTypeTests.swift
+++ PokemonFeed/PokemonFeedTests/Shared/Domain/SuperTypeTests.swift
@@ -5,4 +5,28 @@
 //  Created by Marlon Ansale on 1/7/23.
 //
 
-import Foundation
+import XCTest
+import PokemonFeed
+
+final class SuperTypeTests: XCTestCase {
+    func test_superType_invalid() {
+        let types = ["Trainers","Pokemon","energy","ENERGY",""]
+        
+        types.forEach { type in
+            XCTAssertEqual(SuperType.checkSupertype(type), .unknown)
+        }
+    }
+    
+    func test_superType_valid() {
+        XCTAssertEqual(SuperType.checkSupertype("Trainer"), .trainer)
+        XCTAssertEqual(SuperType.checkSupertype("Pokémon"), .pokemon)
+        XCTAssertEqual(SuperType.checkSupertype("Energy"), .energy)
+    }
+    
+    func test_superTypeString(){
+        XCTAssertEqual(SuperType.unknown.rawValue, "")
+        XCTAssertEqual(SuperType.trainer.rawValue, "Trainer")
+        XCTAssertEqual(SuperType.pokemon.rawValue, "Pokémon")
+        XCTAssertEqual(SuperType.energy.rawValue, "Energy")
+    }
+}
