8b3e9f9a41c56a387e87363e720fa0c9a9417aae
marlon ansale <luffydgreat90@gmail.com>
2023-01-07 17:32:44 +0000

    Added `CardStore`.

---
+++ PokemonFeed/PokemonFeed/Cards/Cache/CardStore.swift
@@ -0,0 +1,33 @@
+//
+//  CardStore.swift
+//  PokemonFeed
+//
+//  Created by Marlon Ansale on 1/7/23.
+//
+
+import Foundation
+
+public typealias CachedCard = (cards: [LocalCard], timestamp: Date)
+
+public protocol CardStore {
+    typealias DeletionResult = Result<Void, Error>
+    typealias DeletionCompletion = (DeletionResult) -> Void
+    
+    typealias InsertionResult = Result<Void, Error>
+    typealias InsertionCompletion = (InsertionResult) -> Void
+
+    typealias RetrievalResult = Result<CachedCard?, Error>
+    typealias RetrievalCompletion = (RetrievalResult) -> Void
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    func deleteCachedCards(completion: @escaping DeletionCompletion)
+
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    func insert(_ cards: [LocalCard], timestamp: Date, completion: @escaping InsertionCompletion)
+
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    func retrieve(completion: @escaping RetrievalCompletion)
+}
