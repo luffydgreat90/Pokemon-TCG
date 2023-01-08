eb1f43ab825658d6bf1a7587cc78522d5d4490cf
marlon ansale <luffydgreat90@gmail.com>
2023-01-08 16:15:19 +0000

    Setup `CardController `.

--- PokemonFeed/PokemoniOS/Cards UI/ViewControllers/CardController.swift
+++ PokemonFeed/PokemoniOS/Cards UI/ViewControllers/CardController.swift
@@ -20,6 +20,15 @@ public final class CardController: NSObject {
         self.delegate = delegate
         self.cell = cell
     }
+    
+    private func cancel() {
+        delegate.didCancelImageRequest()
+        releaseCellForReuse()
+    }
+    
+    private func releaseCellForReuse() {
+        cell = nil
+    }
 }
 
 extension CardController: UICollectionViewDataSource {
@@ -31,6 +40,8 @@ extension CardController: UICollectionViewDataSource {
         cell = collectionView.dequeueReusableCell(indexPath: indexPath)
         cell?.titleLabel.text = viewModel.name
         cell?.priceLabel.text = viewModel.price
+        cell?.cardImageView.image = nil
+        delegate.didRequestImage()
         return cell!
     }
     
@@ -42,17 +53,17 @@ extension CardController: UICollectionViewDataSourcePrefetching {
     }
     
     public func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
-        
+        cancel()
     }
 }
 
 extension CardController: ResourceView, ResourceLoadingView, ResourceErrorView {
     public func display(_ viewModel: UIImage) {
-        
+        cell?.cardImageView.image = viewModel
     }
     
     public func display(_ viewModel: ResourceLoadingViewModel) {
-        
+        cell?.cardImageView.isShimmering = viewModel.isLoading
     }
     
     public func display(_ viewModel: ResourceErrorViewModel) {
