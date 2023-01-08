566535594ea3e331278adc71d02b0dce055f1569
marlon ansale <luffydgreat90@gmail.com>
2023-01-08 16:26:55 +0000

    Fix issue on card call

--- PokemonFeed/PokemonFeed/Cards/Cache/Infrastructure/CoreData/CoreDataCardStore.swift
+++ PokemonFeed/PokemonFeed/Cards/Cache/Infrastructure/CoreData/CoreDataCardStore.swift
@@ -12,30 +12,16 @@ public final class CoreDataCardStore: DataStoreImage {
     public static var model: NSManagedObjectModel = NSManagedObjectModel.with(name: CoreDataCardStore.modelName, in: Bundle(for: CoreDataCardStore.self))!
     
     public static func resultImageRetrieve(dataForURL url: URL, context: NSManagedObjectContext) -> Result<Data?, Error> {
-        if url.absoluteString.contains("small") {
-            return Result {
-                try ManagedCard.first(with: url, argumentArray: [#keyPath(ManagedCard.smallUrl),url], in: context)?.smallData
-            }
-        }else{
-            return Result {
-                try ManagedCard.first(with: url, argumentArray: [#keyPath(ManagedCard.largeUrl),url], in: context)?.largeData
-            }
+        return Result {
+            try ManagedCard.first(with: url, argumentArray: [#keyPath(ManagedCard.smallUrl),url], in: context)?.smallData
         }
     }
     
     public static func resultSaveRetrieve(data: Data, for url: URL, context: NSManagedObjectContext) -> Result<Void, Error> {
-        if url.absoluteString.contains("small") {
-            return Result {
-                try ManagedCard.first(with: url, argumentArray: [#keyPath(ManagedCard.smallUrl),url], in: context)
-                    .map { $0.smallData = data }
-                    .map(context.save)
-            }
-        }else{
-            return Result {
-                try ManagedCard.first(with: url, argumentArray: [#keyPath(ManagedCard.largeUrl),url], in: context)
-                    .map { $0.largeData = data }
-                    .map(context.save)
-            }
+        return Result {
+            try ManagedCard.first(with: url, argumentArray: [#keyPath(ManagedCard.smallUrl),url], in: context)
+                .map { $0.smallData = data }
+                .map(context.save)
         }
     }
     
