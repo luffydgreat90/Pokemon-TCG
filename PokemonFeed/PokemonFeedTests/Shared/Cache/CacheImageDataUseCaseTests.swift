3e86afc287092e87d6429bb5182807d3c4d0d35e
marlon ansale <luffydgreat90@gmail.com>
2023-01-08 22:40:37 +0000

    Added more test on `CacheImageDataUseCaseTests`.

--- PokemonFeed/PokemonFeedTests/Shared/Cache/CacheImageDataUseCaseTests.swift
+++ PokemonFeed/PokemonFeedTests/Shared/Cache/CacheImageDataUseCaseTests.swift
@@ -16,6 +16,46 @@ class CacheImageDataUseCaseTests: XCTestCase {
         XCTAssertTrue(store.receivedMessages.isEmpty)
     }
     
+    func test_saveImageDataForURL_requestsImageDataInsertionForURL() {
+        let (sut, store) = makeSUT()
+        let url = anyURL()
+        let data = anyData()
+
+        sut.save(data, for: url) { _ in }
+
+        XCTAssertEqual(store.receivedMessages, [.insert(data: data, for: url)])
+    }
+    
+    func test_saveImageDataFromURL_failsOnStoreInsertionError() {
+        let (sut, store) = makeSUT()
+
+        expect(sut, toCompleteWith: failed(), when: {
+            let insertionError = anyNSError()
+            store.completeInsertion(with: insertionError)
+        })
+    }
+    
+    func test_saveImageDataFromURL_succeedsOnSuccessfulStoreInsertion() {
+        let (sut, store) = makeSUT()
+
+        expect(sut, toCompleteWith: .success(()), when: {
+            store.completeInsertionSuccessfully()
+        })
+    }
+    
+    func test_saveImageDataFromURL_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+        let store = ImageDataStoreSpy()
+        var sut: LocalImageDataLoader? = LocalImageDataLoader(store: store)
+
+        var received = [LocalImageDataLoader.SaveResult]()
+        sut?.save(anyData(), for: anyURL()) { received.append($0) }
+
+        sut = nil
+        store.completeInsertionSuccessfully()
+
+        XCTAssertTrue(received.isEmpty, "Expected no received results after instance has been deallocated")
+    }
+    
     // MARK: - Helpers
 
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalImageDataLoader, store: ImageDataStoreSpy) {
@@ -24,6 +64,33 @@ class CacheImageDataUseCaseTests: XCTestCase {
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
+    }
+    
+    private func failed() -> LocalImageDataLoader.SaveResult {
+        return .failure(LocalImageDataLoader.SaveError.failed)
+    }
+    
+    private func expect(_ sut: LocalImageDataLoader, toCompleteWith expectedResult: LocalImageDataLoader.SaveResult, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Wait for save completion")
+
+        sut.save(anyData(), for: anyURL()) { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case (.success, .success):
+                break
+
+            case (.failure(let receivedError as LocalImageDataLoader.SaveError),
+                  .failure(let expectedError as LocalImageDataLoader.SaveError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+
+            default:
+                XCTFail("Expected result \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+
+            exp.fulfill()
+        }
+
+        action()
+        wait(for: [exp], timeout: 1.0)
     }
     
 }
