2d1fa53107931a5b2f0029c05aa983cc24282459
marlon ansale <luffydgreat90@gmail.com>
2023-01-10 11:35:47 +0000

    Added `CollectionListSnapshotTests`.

---
+++ PokemonFeed/PokemoniOSTests/Card UI/CardSnapshotTests.swift
@@ -0,0 +1,14 @@
+//
+//  CardSnapshotTests.swift
+//  PokemoniOSTests
+//
+//  Created by Marlon Ansale on 1/10/23.
+//
+
+import XCTest
+import PokemoniOS
+@testable import PokemonFeed
+
+final class CardSnapshotTests: XCTestCase {
+    
+}
