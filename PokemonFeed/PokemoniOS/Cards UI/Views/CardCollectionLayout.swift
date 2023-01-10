3c8ee3da142cf046d2bd8f141057bf62c9b5d20c
marlon ansale <luffydgreat90@gmail.com>
2023-01-10 17:29:45 +0000

    Added `CardCollectionLayout`.

---
+++ PokemonFeed/PokemoniOS/Cards UI/Views/CardCollectionLayout.swift
@@ -0,0 +1,20 @@
+//
+//  CardCollectionLayout.swift
+//  PokemoniOS
+//
+//  Created by Marlon Ansale on 1/10/23.
+//
+
+import UIKit
+
+public final class CardCollectionLayout: UICollectionViewFlowLayout {
+    public override init() {
+        super.init()
+        self.itemSize = CGSize(width:110, height: 200)
+        self.sectionInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
